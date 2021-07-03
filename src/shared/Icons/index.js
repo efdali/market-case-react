887a283c41a30810b9f5c37b174835296ea4fc45
efdalincesu <efdal@hype.com.tr>
2021-07-03 16:32:59 +0300

    svg icon components created

---
+++ src/shared/Icons/index.js
@@ -0,0 +1,11 @@
+import Logo from './Logo';
+import Basket from './Basket';
+import NextArrow from './NextArrow';
+import PrevArrow from './PrevArrow';
+
+export {
+  Logo as LogoSvg,
+  Basket as BasketSvg,
+  NextArrow as NextArrowSvg,
+  PrevArrow as PrevArrowSvg,
+};
